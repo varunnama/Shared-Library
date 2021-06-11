737addb418b47b9096d3d8f0593062abef306427
varunnama <varunnama@gmail.com>
2021-06-11 22:43:46 +0530

    update

--- vars/choiceparameter.groovy
+++ vars/choiceparameter.groovy
@@ -1,1 +1,6 @@
-
+def getBuildProperties() {
+       def buildParameter
+       buildParameter= [choice(choices:['Build Only','Dev','Test'], description: 'Select Correct UCD environment', name: 'Environment')]
+       
+       return buildParameter
+}
