d2a8ff2a7391815361f8ea3390135ff13bc52311
varunnama <varunnama@gmail.com>
2021-06-20 22:02:18 +0530

    updated

--- vars/choiceparameter.groovy
+++ vars/choiceparameter.groovy
@@ -1,7 +1,7 @@
 def getBuildProperties() {
        def buildParameter
-       buildParameter= [input message: 'Select Env for deploy', parameters: [choice(choices: ['UAT', 'STAGE'], description: 'Select correct env for deploy', name: 'Environment')]]
+      // buildParameter= [input message: 'Select Env for deploy', parameters: [choice(choices: ['UAT', 'STAGE'], description: 'Select correct env for deploy', name: 'Environment')]]
                 
-       
+       buildParameter = 'a'
        return buildParameter
 }
